03bdf80ac5a5dccf496ed1322185bc1a519c1cdb
Rebecca Kazinka <rkazinka@gmail.com>
2022-11-28 14:11:19 -0600

    Create qualtrics elements for symmetry span

---
+++ symmetry_span-qualtrics.js
@@ -0,0 +1,66 @@
+// Retrieve Qualtrics object and save in qthis
+var qthis = this;
+
+// Hide buttons
+qthis.hideNextButton();
+
+var task_github = "https://kazinka.github.io/Symmetry-Span/"; // https://<your-github-username>.github.io/<your-experiment-name>
+
+// requiredResources must include all the JS files that .html uses.
+var requiredResources = [
+    task_github + "jspsych-6.0.4/jspsych.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-html-keyboard-response.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-symmetry-judgement-task.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-survey-text.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-instructions.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-fullscreen.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-spatial-span.js",
+    task_github + "jspsych-6.0.4/plugins/jspsych-spatial-span-recall.js",
+    "https://cdn.jsdelivr.net/npm/jstat@latest/dist/jstat.min.js",
+    "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js",
+    task_github + "symmetry_span_main.js"
+];
+
+function loadScript(idx) {
+    console.log("Loading ", requiredResources[idx]);
+    jQuery.getScript(requiredResources[idx], function () {
+        if ((idx + 1) < requiredResources.length) {
+            loadScript(idx + 1);
+        } else {
+            initExp();
+        }
+    });
+}
+
+if (window.Qualtrics && (!window.frameElement || window.frameElement.id !== "mobile-preview-view")) {
+    loadScript(0);
+}
+
+// jQuery is loaded in Qualtrics by default
+jQuery("<div id = 'display_stage_background'></div>").appendTo('body');
+jQuery("<div id = 'display_stage'></div>").appendTo('body');
+
+function initExp(){
+    /* start the experiment*/
+    jsPsych.init({
+        timeline: timeline,
+        /* Change 1: Using `display_element` */
+        display_element: 'display_stage',
+        on_finish: function() {
+            jsPsych.data.displayData(); // comment out if you do not want to display results at the end of task
+            /* Change 5: Summarizing and save the results to Qualtrics */
+            // summarize the results
+            var data = jsPsych.data.get().filter([{trial_type:'symmetry-span-recall'}]);
+
+            // save to qualtrics embedded data
+            Qualtrics.SurveyEngine.setEmbeddedData("data", data);
+            /* Change 6: Adding the clean up and continue functions.*/
+            // clear the stage
+            jQuery('#display_stage').remove();
+            jQuery('#display_stage_background').remove();
+
+            // simulate click on Qualtrics "next" button, making use of the Qualtrics JS API
+            qthis.clickNextButton();
+        }
+      }); 
+    }
