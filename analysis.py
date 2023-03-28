a8cda0d98b24debfb8967c1f196de374d53e0b81
Rebecca Kazinka <rkazinka@gmail.com>
2023-03-28 16:23:33 -0500

    json analysis

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import statistics
+import json
 
 def nearAccuracy(recalled,correct):
     acc=0
@@ -14,13 +15,56 @@ def nearAccuracy(recalled,correct):
 
 ##symetry span##
 #currently makes table with rows based on set size. Do we want just a single row?
-span=pd.read_csv("data/WM_symmetry_span__1679329756389.csv")
+#span=pd.read_csv("data/WM_symmetry_span__1679329756389.csv")
+#f=open("data/sampledata2.json")
+#data=json.load(f)
+#f.close()
 
+#for i in data['displayedFields']:
+#    print(i)
+
+#displayedFields
+#displayedValues
+#labels
+#values
+
+
+with open("data/sampledata2.json") as jsonFile:
+    data=json.load(jsonFile)
+    jsonDataSpan = data['values']['SpatialSpan']
+    jsonDataGrid = data['values']['TrailsAB']
+
+##span##
+#just trying to look at the data more closely
+spanJson = json.loads(jsonDataSpan)
+
+print(json.dumps(spanJson,sort_keys=True, indent=4))
+    
+#attempting to filter
+span_dict = [x for x in spanJson if x['trial_type'] == 'spatial-span-recall']
+
+for x in span_dict:
+    print(x) 
+    keys = x.keys()
+    print(keys)
+    values = x.values()
+    print(values)
+
+span=pd.json_normalize(span_dict)
+
+## based on using pandas instead
 res = pd.DataFrame(columns = ['setsize','meanRT', 'meanAccuracy','meanDistance','meanNearAccuracy'])
 
 #need to turn strings (distance,recall, stimuli) into arrays 
 for n in span.distance:
-   span.dist=span.distance[n].split(',')
+    print(n)
+    #span.dist=span.distance[n].split(',') #nope
+
+#this does not work
+df= span.explode(list('distancerecallstimuli'))
+
+#need to remove first two rows if they did the instructions (i.e., there are 8 rows)
+
 
 for n in [3,4,5]:
     res=res.append({'setsize': n, 'meanRT': statistics.mean(span.rt.loc[span['set_size'] == n]),\
@@ -31,9 +75,22 @@ for n in [3,4,5]:
 
 
 ##grid##
+#just trying to look at the data more closely
+gridJson = json.loads(jsonDataGrid)
+
+grid_dict = [x for x in gridJson if x['trial_type'] == 'trails']
+
+for x in gridJson: 
+    keys = x.keys()
+    print(keys)
+    values = x.values()
+    print(values)
+
+
+grid=pd.json_normalize(grid_dict)
 #currently makes table with each trail (A and B) as row. Do we want a single row?
 
-grid=pd.read_csv("data/Grid_trails_AB_test_1679347624736.csv")
+#grid=pd.read_csv("data/Grid_trails_AB_test_1679347624736.csv")
 
 
 res2= pd.DataFrame(columns = ['trailType','RT', 'numErrors','meanDistance'])
